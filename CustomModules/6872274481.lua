6d7d30607fa24836c7bf4f826159a9ebacecedad
lolthatseazy <105562606+lolthatseazy@users.noreply.github.com>
2022-08-01 03:47:48 -0300

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -8656,8 +8656,7 @@ local AnticheatDisabler2 = {["Enabled"] = false}
 					until (not AnticheatDisabler2["Enabled"])
 				end)
 			end
-		end,
-		["HoverText"] = "Bypasses Anticheat V2"
+		end
 	})
 
 local AnticheatDisabler = {["Enabled"] = false}
@@ -8672,8 +8671,7 @@ local AnticheatDisabler = {["Enabled"] = false}
 					until (not AnticheatDisabler["Enabled"])
 				end)
 			end
-		end,
-		["HoverText"] = "Completly Disables Anticheat Sometimes can flag"
+		end
 	})
 	
 	
